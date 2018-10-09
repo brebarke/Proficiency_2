533d7df05242e4dcc779e021033843f90cccb574
brebarke <brebarke@iu.edu>
2018-10-09 15:19:32 -0400

    Initial Commit

---
+++ Proficiency_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proficiency_2
+//
+//  Created by Brett Barker on 10/9/18.
+//  Copyright © 2018 Brett Barker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
