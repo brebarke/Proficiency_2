ec75da626bb653aa01032f83d365b8c484651274
brebarke <brebarke@iu.edu>
2018-10-18 15:03:03 -0400

    Working calculator

--- Proficiency_2/ViewController.swift
+++ Proficiency_2/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var answerLabel: UILabel!
+    var num1 = ""
+    var num2 = ""
+    var operation = ""
+    var isnum1 = true
+    var operationpresent = false
+    var clear = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func buttonPress(_ sender: UIButton) {
+        if clear {
+            answerLabel.text = ""
+            clear = false
+        }
+        let curText = answerLabel.text!
+        let textLabel = sender.titleLabel?.text
+        if let text = textLabel {
+            switch text {
+            case "+", "x", "/", "-":
+                if operationpresent {
+                    return
+                }
+                operation = text
+                isnum1 = false
+                operationpresent = true
+                answerLabel.text = "\(curText) \(operation) "
+                break
+            case "=":
+                isnum1 = true
+                operationpresent = false
+                clear = true
+                let result = calculate()
+                answerLabel.text = "\(result)"
+                break
+            default:
+                if isnum1 {
+                    num1 = "\(num1)\(text)"
+                } else {
+                    num2 = "\(num2)\(text)"
+                }
+                answerLabel.text = "\(curText)\(text)"
+                break;
+        
+        }
+    }
+}
+func calculate() -> Double {
+    let firstNum = Double(num1)!
+    let secondNum = Double(num2)!
+    num1 = ""
+    num2 = ""
+    switch operation {
+    case "+":
+        return firstNum + secondNum
+    case "-":
+        return firstNum - secondNum
+    case "x":
+        return firstNum * secondNum
+    case "/":
+        return firstNum / secondNum
+    default:
+        return 0
+        }
+    }
 
+    
 
 }
 
+//resource: https://github.com/fnk0/iOS-Calculator-Tutorial
